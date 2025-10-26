6689f5d296bac0c565d3d9943559fbc49b13bb93
nam <29ngtrung@gmail.com>
2025-10-26 19:22:48 +0700

    Test thử chức năng swipe

--- app/src/main/java/com/btl/tinder/MainActivity.kt
+++ app/src/main/java/com/btl/tinder/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.btl.tinder
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import com.btl.tinder.ui.SwipeCards
+import com.btl.tinder.ui.theme.TinderCloneTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            TinderCloneTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SwipeCards()
+                }
+            }
         }
     }
 }
