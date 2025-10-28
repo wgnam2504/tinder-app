cad1026d4c37c5bd45fa495ebcd19687c8ffccef
ngtrung <29ngtrung@gmail.com>
2025-10-28 10:58:54 +0700

    Add route vào sealed class

--- app/src/main/java/com/btl/tinder/ui/ProfileScreen.kt
+++ app/src/main/java/com/btl/tinder/ui/ProfileScreen.kt
@@ -6,6 +6,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 
+sealed class DestinationScreen(val route: String) {
+    object Signup : DestinationScreen("signup")
+    object Login : DestinationScreen("login")
+    object Profile : DestinationScreen("profile")
+    object Swipe : DestinationScreen("swipe")
+    object ChatList : DestinationScreen("chatList")
+    object SingleChat : DestinationScreen("singleChat/{chatId}") {
+        fun createRoute(id: String) = "singleChat/$id"
+    }
+}
+
 @Composable
 fun ProfileScreen() {
     Column(
